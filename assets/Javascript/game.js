11441e79ff56f4704adc4dd17f2ae4581e237138
Zaid Alhir <zaidalhir@yahoo.com>
2019-11-09 10:39:49 -0600

    added firebase

---
+++ assets/Javascript/game.js
@@ -0,0 +1,16 @@
+
+
+  // Web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBriYEsnpax18nummPt1PEm5rN6qD0qxx8",
+    authDomain: "forclasse-7f050.firebaseapp.com",
+    databaseURL: "https://forclasse-7f050.firebaseio.com",
+    projectId: "forclasse-7f050",
+    storageBucket: "forclasse-7f050.appspot.com",
+    messagingSenderId: "1001175965488",
+    appId: "1:1001175965488:web:6923c8aca42b8251e732f5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  console.log(firebase)
